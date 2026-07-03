14ae44a6dfcd7a7c34f085874221783a96aa1902
Thompson Lane <thompsonplane@gmail.com>
2026-07-03 14:36:56 -0500

    More changes from demo

--- src/tui/main.rs
+++ src/tui/main.rs
@@ -1,5 +1,9 @@
 use std::io;
 
+use color_eyre::{
+    eyre::{bail, WrapErr},
+    Result
+};
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     buffer::Buffer,
@@ -11,8 +15,12 @@ use ratatui::{
     DefaultTerminal, Frame,
 };
 
-fn main() -> io::Result<()> {
-    ratatui::run(|terminal| App::default().run(terminal))
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
 
 #[derive(Debug, Default)]
@@ -22,10 +30,10 @@ pub struct App {
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> color_eyre::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
+            self.handle_events().wrap_err("handle events failed")?;
         }
         Ok(())
     }
@@ -34,11 +42,11 @@ impl App {
         frame.render_widget(self, frame.area());
     }
 
-    fn handle_events(&mut self) -> io::Result<()> {
+    fn handle_events(&mut self) -> color_eyre::Result<()> {
         match event::read()? {
             Event::Key(key_event) => {
                 if key_event.kind == KeyEventKind::Press {
-                    self.handle_key_event(key_event)
+                    self.handle_key_event(key_event).wrap_err_with(|| format!("handling key event failed:\n{key_event:#?}"))?;
                 }
             }
             _ => {}
@@ -46,12 +54,15 @@ impl App {
         Ok(())
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> color_eyre::Result<()> {
         match key_event.code {
-            KeyCode::Char('q') => self.exit(),
+            KeyCode::Char('q') => {
+                self.exit();
+                Ok(())
+            }
             KeyCode::Left => self.decrement_counter(),
             KeyCode::Right => self.increment_counter(),
-            _ => {}
+            _ => Ok(())
         }
     }
 
@@ -59,12 +70,17 @@ impl App {
         self.exit = true;
     }
 
-    fn increment_counter(&mut self) {
+    fn increment_counter(&mut self) -> color_eyre::Result<()> {
         self.counter += 1;
+        if self.counter > 3 {
+            bail!("counter overflow");
+        }
+        Ok(())
     }
 
-    fn decrement_counter(&mut self) {
+    fn decrement_counter(&mut self) -> color_eyre::Result<()> {
         self.counter -= 1;
+        Ok(())
     }
 }
 
